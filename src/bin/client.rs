9070e852aadee5d1291f49fea95edeab77aa9cdc
Mark Mansi <markm@cs.wisc.edu>
2020-06-11 10:08:26 -0500

    fix job ls suffix bug

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -829,7 +829,7 @@ fn list_jobs(addr: &str, mode: JobListMode) -> Vec<JobInfo> {
                 .enumerate()
                 .filter(|(i, j)| match mode {
                     JobListMode::All => true,
-                    JobListMode::Suffix(n) => *i >= len - n,
+                    JobListMode::Suffix(n) => (*i >= len - n) || (len <= n),
                     JobListMode::Jids(ref jids) => jids.contains(j),
                 })
                 .map(|(_, j)| j)
